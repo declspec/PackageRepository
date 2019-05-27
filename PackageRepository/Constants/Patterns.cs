dd21dbd858c35cb8b32bfa443cee4fc246ddd04f
declspec <jasonlarke@live.com.au>
2019-05-27 17:45:38 +0800

    Added missing `?` from semver regex

--- PackageRepository/Constants/Patterns.cs
+++ PackageRepository/Constants/Patterns.cs
@@ -2,6 +2,6 @@
     public static class Patterns {
         public const string OrganisationName = "(?<organisation>[a-z0-9-][a-z0-9_.-]*)";
         public const string PackageName = @"(?<package>(?:@(?<scope>[a-z0-9-][a-z0-9_.-]*)(?:/|%2[fF]))?(?<name>[a-z0-9-][a-z0-9_.-]*))";
-        public const string SemVer = @"(?<version>(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<identifier>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(?:\+[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)";
+        public const string SemVer = @"(?<version>(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<identifier>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(?:\+[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*)?)?)";
     }
 }
